34a9a24d6698afedbdfb24b89657324c04018a68
MandelaK <kazi.keith@gmail.com>
2018-11-22 23:30:00 +0300

    chore(configuration): update database configuration
    update database configuration and add method for creating and deleting admins
    
    [#162030496]

--- db_config.py
+++ db_config.py
@@ -1,6 +1,7 @@
 import os
 
 import psycopg2
+from werkzeug.security import generate_password_hash
 
 from instance.config import app_config
 
@@ -24,7 +25,7 @@ def tables():
         first_name varchar (25) NOT NULL,
         last_name varchar (25) NOT NULL,
         email varchar(50) NOT NULL unique,
-        password varchar (64) NOT NULL,
+        password varchar (254) NOT NULL,
         phone bigint unique,
         is_admin boolean DEFAULT 'f'
         )"""
@@ -60,10 +61,39 @@ def create_tables():
 def destroy_tables():
     conn = init_db()
     cursor = conn.cursor()
-    drop_users = """DROP TABLE IF EXISTS users CASCADE"""
-    drop_parcels = """DROP TABLE IF EXISTS parcels CASCADE"""
+    drop_users = """DROP TABLE IF EXISTS users CASCADE;"""
+    drop_parcels = """DROP TABLE IF EXISTS parcels CASCADE;"""
 
     queries = [drop_users, drop_parcels]
     for query in queries:
         cursor.execute(query)
+    conn.commit()
+
+
+def create_admin():
+    """We will import this method and initialize it after we create our
+    tables so that we always have admin in the database."""
+
+    conn = init_db()
+    cursor = conn.cursor()
+
+    admin_pass = {"password": generate_password_hash("adminpassword")}
+    create_admin = """
+    INSERT INTO users (first_name, last_name, email, password, phone, is_admin)
+    VALUES ('Admin', 'Major', 'admin@admin.admin', %(password)s, 1111111111, 't')
+    """
+    cursor.execute(create_admin, admin_pass)
+    print("Creating admin")
+    conn.commit()
+
+
+def delete_admin():
+    """We delete admin before creating our app so that there can only always be only
+    one admin"""
+
+    conn = init_db()
+    cursor = conn.cursor()
+    delete_ad = """DELETE FROM users WHERE email = 'admin@admin.admin'
+    """
+    cursor.execute(delete_ad)
     conn.commit()
