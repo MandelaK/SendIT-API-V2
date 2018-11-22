47d98d2cf3447cd43bfea65666d9514a1a5e9bd9
MandelaK <kazi.keith@gmail.com>
2018-11-22 23:36:08 +0300

    chore(configuration): update project configuration
    update configuration files so that admins are created each time app starts. Also allow configuration variable to be passed when starting the app
    
    [#162030496]

--- app/__init__.py
+++ app/__init__.py
@@ -2,15 +2,20 @@
 from flask import Flask
 # we import created blueprints to register them
 from .api.v2 import version2
+from db_config import create_tables, create_admin, delete_admin
+from instance.config import app_config
 
 
-def create_app():
+def create_app(config_name):
     """This is our app factory from where we register our app and
     API version"""
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_pyfile('config.py')
+    app.config.from_object(app_config[config_name])
     # we register the blueprint
     app.register_blueprint(version2)
     # register more blueprints in a similar manner
+    create_tables()
+    delete_admin()
+    create_admin()
 
     return app
