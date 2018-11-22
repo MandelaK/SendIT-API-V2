128e7f5e0e71b73278e1d3bd7bc4e5fb224bd449
MandelaK <kazi.keith@gmail.com>
2018-11-22 23:32:07 +0300

    chore(configuration): update config.py file
    add more environments for different configuration types and add unique database url for each configuration
    
    [#162030496]

--- instance/config.py
+++ instance/config.py
@@ -1,5 +1,29 @@
+import os
+
 class Config():
-    """Configuration class"""
+    """Main configuration class"""
+    DEBUG = False
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+
+class Development(Config):
+    """Configuration for development"""
     DEBUG = True
-    CSRF_ENABLED = True
-SECRET = "Pareanooyd PEEple l0ve Strong PSEEWrods"
+    DATABASE_URL = os.getenv('DATABASE_URL')
+
+class Testing(Config):
+    """Configuration for testing"""
+    DEBUG = True
+    TESTING = True
+    DATABASE_URL = os.getenv('DATABASE_TEST_URL')
+
+class Production(Config):
+    """Configuration for production"""
+    DEBUG = False
+    DATABASE_URL = os.getenv('DATABASE_PRODUCTION_URL')
+
+app_config = {
+    'development': Development,
+    'testing': Testing,
+    'production': Production,
+    'default': Development
+}
