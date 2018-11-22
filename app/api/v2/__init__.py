eb651a1366f2bf3ded6ce48e0109de91b5adebd2
MandelaK <kazi.keith@gmail.com>
2018-11-23 02:25:03 +0300

    feat(functionality): users can create parcels
    users can now create parcels for delivery so long as they are logged in and they provide necessary information.
    
    [starts #162031550]

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 from flask_restful import Api
 
 from app.api.v2.views.user_views import SignupView, LoginView
+from app.api.v2.views.parcel_views import ParcelCreate
 
 version2 = Blueprint("v2", __name__, url_prefix="/api/v2")
 api2 = Api(version2, catch_all_404s=True)
@@ -9,3 +10,4 @@ api2 = Api(version2, catch_all_404s=True)
 
 api2.add_resource(SignupView, "/auth/signup")
 api2.add_resource(LoginView, "/auth/login")
+api2.add_resource(ParcelCreate, "/users/parcels")
