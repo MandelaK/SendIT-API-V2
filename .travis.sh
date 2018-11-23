5857896bb97ffecc94b559db161c7462e6a8a8c8
Cray <44587521+MandelaK@users.noreply.github.com>
2018-11-23 05:40:46 +0300

    create .travis.sh file
    create .travis.sh file in order to pass secret key and environment variable to travis

---
+++ .travis.sh
@@ -0,0 +1,7 @@
+export FLASK_APP="run.py"
+export FLASK_ENV="testing"
+export FLASK_DEBUG=1
+export JWT_SECRET_KEY="THidasfaasecanli87wlylasdfyae03rqeh"
+export DATABASE_URL="dbname='sendit_db' user='postgres' password='' host='localhost' port='5432'"
+export DATABASE_TEST_URL="dbname='travis_ci_test' user='postgres' password='' host='localhost' port='5432'"
+export DATABASE_PRODUCTION_URL="dbname='send_test' user='postgres' password='' host='localhost' port='5432'"
