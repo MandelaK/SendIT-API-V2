3779616f646cc2598e9aaeb783db57efd46ea382
MandelaK <kazi.keith@gmail.com>
2018-11-23 05:33:01 +0300

    feat(functionality): add feature for changing destination
    add views and models for changing destination and ensure they pass previoulsy failing tests
    
    [#162030635]

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from flask_restful import Api
 
 from app.api.v2.views.user_views import SignupView, LoginView
-from app.api.v2.views.parcel_views import ParcelCreate
+from app.api.v2.views.parcel_views import ParcelCreate, ParcelDestination
 
 version2 = Blueprint("v2", __name__, url_prefix="/api/v2")
 api2 = Api(version2, catch_all_404s=True)
@@ -11,3 +11,4 @@ api2 = Api(version2, catch_all_404s=True)
 api2.add_resource(SignupView, "/auth/signup")
 api2.add_resource(LoginView, "/auth/login")
 api2.add_resource(ParcelCreate, "/users/parcels")
+api2.add_resource(ParcelDestination, "/parcels/<int:parcel_id>/destination")
