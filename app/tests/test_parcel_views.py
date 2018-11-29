eeba731800921b27445a0c43eb6a7a0df237148a
MandelaK <kazi.keith@gmail.com>
2018-11-29 14:01:00 +0300

    chore(tests): add tests for getting parcel by ID feature
    add tests for getting parcel by ID feature. They should fail because the functionality has not yet been implemented
    
    [starts #162164202]

--- app/tests/test_parcel_views.py
+++ app/tests/test_parcel_views.py
@@ -5,6 +5,7 @@ from app.tests import BaseTestClass
 
 class TestParcelView(BaseTestClass):
     """"""
+<<<<<<< Updated upstream
 
     def test__create_order(self):
         """This will test POST /parcels"""
@@ -434,3 +435,291 @@ class TestParcelView(BaseTestClass):
         self.assertEqual(result["Error"],
                          "You can only cancel parcels you created")
         self.assertEqual(res.status_code, 401)
+=======
+    self.client.post(
+        "api/v2/users/parcels", data=(json.dumps(self.generic_parcel)),
+        content_type="application/json", headers=self.headers)
+
+    self.client.post("/api/v2/auth/signup",
+                     data=json.dumps(self.generic_user),
+                     content_type="application/json")
+    log = self.client.post("/api/v2/auth/login",
+                           data=json.dumps(self.generic_user_details),
+                           content_type="application/json")
+    logs = json.loads(log.get_data(as_text=True))
+    log_token = logs["token"]
+    temp_headers = {"AUTHORIZATION": "Bearer " + log_token}
+
+    res = self.client.get("/api/v2/parcels", headers=temp_headers)
+    self.assertEqual(res.status_code, 404)
+
+  def test_user_change_status(self):
+    """User should not be able to change the status of deliveries"""
+
+    self.client.post(
+        "api/v2/users/parcels", data=(json.dumps(self.generic_parcel)),
+        content_type="application/json", headers=self.headers)
+
+    status = {"status": "transit"}
+    res = self.client.put("/api/v2/parcels/1/status", data=json.dumps(
+        status), content_type="application/json", headers=self.headers)
+    result = json.loads(res.data)
+    self.assertEqual(result["Forbidden"],
+                     "Only admins can change status of parcels")
+    self.assertEqual(res.status_code, 403)
+
+  def test_admin_change_status(self):
+    """Admins should be able to change status of parcels that are not delivered or cancelled"""
+
+    self.client.post(
+        "api/v2/users/parcels", data=(json.dumps(self.generic_parcel)),
+        content_type="application/json", headers=self.headers)
+
+    status = {"status": "transit"}
+    res = self.client.put("/api/v2/parcels/1/status", data=json.dumps(
+        status), content_type="application/json", headers=self.admin_header)
+    result = json.loads(res.data)
+    self.assertEqual(result["Success"],
+                     "The status for parcel number 1 was successfully changed")
+    self.assertEqual(res.status_code, 200)
+
+  def test_admin_change_invalid_status(self):
+    """Admin should only be able to change status to being on transit or delivered"""
+
+    self.client.post(
+        "api/v2/users/parcels", data=(json.dumps(self.generic_parcel)),
+        content_type="application/json", headers=self.headers)
+
+    status = {"status": "invalid"}
+    res = self.client.put("/api/v2/parcels/1/status", data=json.dumps(
+        status), content_type="application/json", headers=self.admin_header)
+    result = json.loads(res.data)
+    self.assertEqual(result["Error"],
+                     "Status can only be changed to 'transit' or 'delivered'.")
+    self.assertEqual(res.status_code, 400)
+
+  def test_admin_change_status_of_nonexistent_parcel(self):
+    """Admin should only change status of parcels that exist"""
+
+    status = {"status": "delivered"}
+    res = self.client.put("/api/v2/parcels/5/status", data=json.dumps(
+        status), content_type="application/json", headers=self.admin_header)
+    result = json.loads(res.data)
+    self.assertEqual(result["Error"],
+                     "Parcel not found.")
+    self.assertEqual(res.status_code, 404)
+
+  def test_admin_change_status_of_delivered_parcels(self):
+    """Admin should not be able to change status of parcels that have been cancelled
+    or delivered"""
+
+    status = {"status": "delivered"}
+    self.client.put("/api/v2/parcels/1/status", data=json.dumps(
+        status), content_type="application/json", headers=self.admin_header)
+    new_status = {"status": "transit"}
+    res = self.client.put("/api/v2/parcels/1/status", data=json.dumps(
+        new_status), content_type="application/json", headers=self.admin_header)
+    result = json.loads(res.data)
+    self.assertEqual(result["Error"],
+                     "Status cannot be changed for delivered or cancelled parcels")
+    self.assertEqual(res.status_code, 400)
+
+  def test_user_can_change_location(self):
+    """Users should not be able to change location of parcels"""
+
+    self.client.post(
+        "api/v2/users/parcels", data=(json.dumps(self.generic_parcel)),
+        content_type="application/json", headers=self.headers)
+
+    location = {"current_location": "invalid"}
+    res = self.client.put("/api/v2/parcels/1/presentLocation", data=json.dumps(
+        location), content_type="application/json", headers=self.headers)
+    result = json.loads(res.data)
+    self.assertEqual(result["Forbidden"],
+                     "Only admins can update the present location of a parcel.")
+    self.assertEqual(res.status_code, 403)
+
+  def test_admin_can_change_current_location(self):
+    """Admins should be able to update current location of parcels in transit"""
+
+    self.client.post(
+        "api/v2/users/parcels", data=(json.dumps(self.generic_parcel)),
+        content_type="application/json", headers=self.headers)
+
+    status = {"status": "transit"}
+    res = self.client.put("/api/v2/parcels/1/status", data=json.dumps(
+        status), content_type="application/json", headers=self.admin_header)
+    location = {"current_location": "Nairoberry"}
+    res = self.client.put("/api/v2/parcels/1/presentLocation", data=json.dumps(
+        location), content_type="application/json", headers=self.admin_header)
+    result = json.loads(res.data)
+
+    self.assertEqual(result["Success"],
+                     "Successfully updated current location")
+    self.assertEqual(res.status_code, 200)
+
+  def test_admin_can_change_location_of_nonexistent_parcel(self):
+    """Admin should not be able to change location of parcels that don't exist"""
+
+    location = {"current_location": "Nairoberry"}
+    res = self.client.put("/api/v2/parcels/5/presentLocation", data=json.dumps(
+        location), content_type="application/json", headers=self.admin_header)
+    result = json.loads(res.data)
+
+    self.assertEqual(result["Error"],
+                     "Parcel not found")
+    self.assertEqual(res.status_code, 404)
+
+  def test_admin_can_change_location_of_pending_parcels(self):
+    """Admins should not be able to change current location of parcels not in transit"""
+
+    self.client.post(
+        "api/v2/users/parcels", data=(json.dumps(self.generic_parcel)),
+        content_type="application/json", headers=self.headers)
+
+    location = {"current_location": "Nairoberry"}
+    res = self.client.put("/api/v2/parcels/1/presentLocation", data=json.dumps(
+        location), content_type="application/json", headers=self.admin_header)
+    result = json.loads(res.data)
+
+    self.assertEqual(result["Error"],
+                     "You can only change location of parcels in transit")
+    self.assertEqual(res.status_code, 400)
+
+  def test_admin_can_add_invalid_current_location(self):
+    """Admins should not be able to add current locations that are not valid"""
+
+    self.client.post(
+        "api/v2/users/parcels", data=(json.dumps(self.generic_parcel)),
+        content_type="application/json", headers=self.headers)
+
+    status = {"status": "transit"}
+    res = self.client.put("/api/v2/parcels/1/status", data=json.dumps(
+        status), content_type="application/json", headers=self.admin_header)
+    location = {"current_location": "      "}
+    res = self.client.put("/api/v2/parcels/1/presentLocation", data=json.dumps(
+        location), content_type="application/json", headers=self.admin_header)
+    result = json.loads(res.data)
+
+    self.assertEqual(result["Error"],
+                     "Please enter a valid location")
+    self.assertEqual(res.status_code, 400)
+
+  def test_admin_can_cancel_parcel(self):
+    """Admin should not be able to cancel parcels"""
+
+    self.client.post(
+        "api/v2/users/parcels", data=(json.dumps(self.generic_parcel)),
+        content_type="application/json", headers=self.headers)
+
+    res = self.client.put("/api/v2/parcels/1/cancel",
+                          headers=self.admin_header)
+    result = json.loads(res.data)
+
+    self.assertEqual(result["Forbidden"],
+                     "Admins cannot cancel parcels")
+    self.assertEqual(res.status_code, 403)
+
+  def test_user_can_cancel_pending_parcel(self):
+    """User should be able to cancel pending or parcels in transit"""
+
+    self.client.post(
+        "api/v2/users/parcels", data=(json.dumps(self.generic_parcel)),
+        content_type="application/json", headers=self.headers)
+
+    res = self.client.put("/api/v2/parcels/1/cancel",
+                          headers=self.headers)
+    result = json.loads(res.data)
+
+    self.assertEqual(result["Success"],
+                     "Successfully cancelled your parcel")
+    self.assertEqual(res.status_code, 200)
+
+  def test_user_can_cancel_cancelled_parcel(self):
+    """User should not be able to cancel cancelled or delivered parcels"""
+
+    self.client.post(
+        "api/v2/users/parcels", data=(json.dumps(self.generic_parcel)),
+        content_type="application/json", headers=self.headers)
+
+    self.client.put("/api/v2/parcels/1/cancel",
+                    headers=self.headers)
+    res = self.client.put("/api/v2/parcels/1/cancel",
+                          headers=self.headers)
+    result = json.loads(res.data)
+
+    self.assertEqual(result["Error"],
+                     "You can only cancel parcels in transit")
+    self.assertEqual(res.status_code, 400)
+
+  def test_user_can_cancel_nonexistent_parcels(self):
+    """User should not be able to cancel parcels that don't exist"""
+
+    res = self.client.put("/api/v2/parcels/5/cancel",
+                          headers=self.headers)
+    result = json.loads(res.data)
+
+    self.assertEqual(result["Error"],
+                     "Parcel not found")
+    self.assertEqual(res.status_code, 404)
+
+  def test_user_can_cancel_parcel_by_another_user(self):
+    """User should not be able to cancel parcels they did no create"""
+
+    self.client.post(
+        "api/v2/users/parcels", data=(json.dumps(self.generic_parcel)),
+        content_type="application/json", headers=self.headers)
+
+    self.client.post("/api/v2/auth/signup", data=json.dumps(self.generic_user),
+                     content_type="application/json")
+    log = self.client.post("/api/v2/auth/login", data=json.dumps(self.generic_user_details),
+                           content_type="application/json")
+    logs = json.loads(log.get_data(as_text=True))
+    log_token = logs["token"]
+    temp_headers = {"AUTHORIZATION": "Bearer " + log_token}
+
+    res = self.client.put("/api/v2/parcels/1/cancel",
+                          headers=temp_headers)
+    result = json.loads(res.data)
+    self.assertEqual(result["Error"],
+                     "You can only cancel parcels you created")
+    self.assertEqual(res.status_code, 401)
+
+  def test_admin_can_get_parcel_by_id(self):
+    """Admins should be able to get any parcel by their ID if the parcels exist"""
+
+    res = self.client.get("api/v2/parcels/1", headers=self.admin_header)
+    # result = json.loads(res.json)
+
+    # self.assertEqual(result["Parcel 1"], )
+    self.assertEqual(res.status_code, 200)
+
+  def test_get_nonexistent_parcel_by_id(self):
+    """Admins and users should be notified when they try to get non-existent parcels"""
+
+    res = self.client.get(
+        "api/v2/parcels/44", content_type="application/json", headers=self.admin_header)
+    result = json.loads(res.data)
+
+    self.assertEqual(result["Error"], "Parcel 44 does not exist")
+    self.assertEqual(res.status_code, 404)
+
+  def test_user_get_parcel_that_is_not_theirs_by_id(self):
+    """Users should not be able to get parcels that are not theirs by ID."""
+
+    self.client.post("/api/v2/auth/signup", data=json.dumps(self.generic_user),
+                     content_type="application/json")
+    log = self.client.post("/api/v2/auth/login", data=json.dumps(self.generic_user_details),
+                           content_type="application/json")
+    logs = json.loads(log.get_data(as_text=True))
+    log_token = logs["token"]
+    temp_headers = {"AUTHORIZATION": "Bearer " + log_token}
+
+    res = self.client.get(
+        "api/v2/parcels/1", content_type="application/json", headers=temp_headers)
+    result = json.loads(res.data)
+
+    self.assertEqual(
+        result["Error"], "You can only view your own parcels. To view them, search for all your parcels and then use the parcel_id provided by that request in this link.")
+    self.assertEqual(res.status_code, 403)
+>>>>>>> Stashed changes
