03db9bfac876d09d8223d43da7c110aea4d2a9f3
MandelaK <kazi.keith@gmail.com>
2018-11-29 14:03:43 +0300

    chore(register views): register views for handling requests to get parcel by ID
    register views to handle requests for getting parcels by ID.
    
    [finishes #162164202]

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -3,7 +3,7 @@ from flask_restful import Api
 
 from app.api.v2.views.user_views import SignupView, LoginView
 from app.api.v2.views.parcel_views import (
-    ParcelCreate, ParcelDestination, ParcelView, ParcelStatus, ParcelLocation, CancelParcel)
+    ParcelCreate, ParcelDestination, ParcelView, ParcelStatus, ParcelLocation, CancelParcel, SpecificParcel)
 
 version2 = Blueprint("v2", __name__, url_prefix="/api/v2")
 api2 = Api(version2, catch_all_404s=True)
@@ -17,3 +17,4 @@ api2.add_resource(ParcelLocation, "/parcels/<int:parcel_id>/presentLocation")
 api2.add_resource(ParcelView, "/parcels")
 api2.add_resource(ParcelStatus, "/parcels/<int:parcel_id>/status")
 api2.add_resource(CancelParcel, "/parcels/<int:parcel_id>/cancel")
+api2.add_resource(SpecificParcel, "/parcels/<int:parcel_id>")
