0764dda7e15ac137f1cc9623d488faf75491169a
MandelaK <kazi.keith@gmail.com>
2019-01-13 08:41:17 +0300

    chore(cnofiguration): set up flask-cors
    set up and configure flask-cors so that all our routes accept Fetch requests from JavaScript
    
    [starts #163175396]
    [finishes #163175396]

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 
 from flask import Flask
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 # we import created blueprints to register them
+
 from .api.v2 import version2
 from db_config import create_tables, create_admin
 from instance.config import app_config
@@ -17,6 +19,7 @@ def create_app(config_name):
     # we register the blueprint
     app.register_blueprint(version2)
     jwt.init_app(app)
+    CORS(app)
     # register more blueprints in a similar manner
     create_tables()
     # delete_admin()
